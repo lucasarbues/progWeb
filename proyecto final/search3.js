8c35fd9966b0a57fe2d436062ef7c2d02f6c09ca
lucasarbues <79850409+lucasarbues@users.noreply.github.com>
2022-11-22 15:15:46 +0100

    Create search3.js

---
+++ proyecto final/search3.js
@@ -0,0 +1,69 @@
+// add event listener
+filterInput.addEventListener('keyup', filterProducts);
+
+// callback function 
+function filterProducts(){
+    let filterValue = filterInput.value.toUpperCase();
+    let item = equipos.querySelectorAll('.caja')
+    // console.log(filterValue);
+
+    for (let i = 0; i < item.length; i++){
+        let span = item[i].querySelector('.title');
+
+        if(span.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+            item[i].style.display = "initial";
+        }else{
+            item[i].style.display = "none";
+        }
+
+    }
+
+}
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("caja");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) == -1) {
+        element.className += " " + arr2[i];
+      }
+    }
+  }
+  
+  // Hide elements that are not selected
+  function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      while (arr1.indexOf(arr2[i]) > -1) {
+        arr1.splice(arr1.indexOf(arr2[i]), 1);
+      }
+    }
+    element.className = arr1.join(" ");
+  }
+  
+  // Add active class to the current control button (highlight it)
+  var btnContainer = document.getElementById("myBtnContainer");
+  var btns = btnContainer.getElementsByClassName("btn");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
